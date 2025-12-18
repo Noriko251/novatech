7ce49cc437fe5ea71371b749ab5b12cdc9f415ec
Noriko Taniguchi <studio-nonko@hotmail.co.jp>
2025-12-18 22:52:05 +0900

    second commit

--- js/frontPage.js
+++ js/frontPage.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   /* ここから記述します。*/
 
+  // Fade In-------------------------------------------------
   const targets = document.querySelectorAll(".fadein");
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
-        observer.unobserve(entry.target); // 1回だけ実行したい場合
+        observer.unobserve(entry.target);
       }
     });
   });
 
   targets.forEach((target) => observer.observe(target));
 
+  // To Top Button--------------------------------------------
   const toTopBtn = document.getElementById("top__icon");
   const showPosition = 300;
 
